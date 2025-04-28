1736cefa82aa61d69cfa4d807c11c87f3e5db5f9
AnhNT <anhntse183225@fpt.edu.vn>
2025-04-29 04:44:43 +0700

    Init

---
+++ src/main/java/com/flowfy/artline/ArtlineBeApplication.java
@@ -0,0 +1,13 @@
+package com.flowfy.artline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtlineBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtlineBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flowfy/artline/ArtlineBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowfy.artline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtlineBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
